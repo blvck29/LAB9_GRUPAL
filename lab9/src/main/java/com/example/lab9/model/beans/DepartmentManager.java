3231517792347c8fca49955a73c37c5826f5d30b
blvck29 <a20210850@pucp.edu.pe>
2023-10-29 08:21:54 -0500

    dao

---
+++ lab9/src/main/java/com/example/lab9/model/beans/DepartmentManager.java
@@ -0,0 +1,60 @@
+package com.example.lab9.model.beans;
+
+import java.sql.Date;
+
+public class DepartmentManager {
+    private int emp_no;
+    private String emp_fullname;
+    private String dept_no;
+    private String dept_name;
+    private Date from_date;
+    private Date to_date;
+
+    public int getEmp_no() {
+        return emp_no;
+    }
+
+    public void setEmp_no(int emp_no) {
+        this.emp_no = emp_no;
+    }
+
+    public String getEmp_fullname() {
+        return emp_fullname;
+    }
+
+    public void setEmp_fullname(String emp_fullname) {
+        this.emp_fullname = emp_fullname;
+    }
+
+    public String getDept_no() {
+        return dept_no;
+    }
+
+    public void setDept_no(String dept_no) {
+        this.dept_no = dept_no;
+    }
+
+    public String getDept_name() {
+        return dept_name;
+    }
+
+    public void setDept_name(String dept_name) {
+        this.dept_name = dept_name;
+    }
+
+    public Date getFrom_date() {
+        return from_date;
+    }
+
+    public void setFrom_date(Date from_date) {
+        this.from_date = from_date;
+    }
+
+    public Date getTo_date() {
+        return to_date;
+    }
+
+    public void setTo_date(Date to_date) {
+        this.to_date = to_date;
+    }
+}
---
+++ lab9/src/main/java/com/example/lab9/model/daos/DeptManDao.java
@@ -0,0 +1,58 @@
+package com.example.lab9.model.daos;
+
+import com.example.lab9.model.beans.DepartmentEmp;
+import com.example.lab9.model.beans.DepartmentManager;
+import com.example.lab9.model.beans.Employee;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DeptManDao {
+    public ArrayList<Employee> listarDeptMan () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Parámetros de Conexión
+        String url = "jdbc:mysql://localhost:3306/employees";
+        String username = "root";
+        String password = "root";
+
+        //Conexión a la DB
+        String sql = "SELECT e.emp_no, concat(e.first_name, \" \", e.last_name) as `full_name`, d.dept_no, ddd.dept_name, d.from_date, d.to_date\n" +
+                "\tFROM dept_manager d\n" +
+                "\tINNER JOIN employees e on (d.emp_no = e.emp_no)\n" +
+                "    LEFT JOIN departments ddd on (d.dept_no = ddd.dept_no)";
+
+        ArrayList<DepartmentManager> listaDeptMan = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+
+                DepartmentManager deptMan = new DepartmentManager();
+
+                deptMan.setEmp_no(rs.getInt(1));
+                deptMan.setEmp_fullname(rs.getString(2));
+                deptMan.setDept_no(rs.getString(3));
+                deptMan.setDept_name(rs.getString(4));
+                deptMan.setFrom_date(rs.getDate(5));
+                deptMan.setTo_date(rs.getDate(6));
+
+                listaDeptMan.add(deptMan);
+
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return listarDeptMan();
+    }
+
+}
